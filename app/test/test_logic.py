6628b3104b8b626e4fa269bb6ef56cac3110e159
Dudu <dudidoo265@gmail.com>
2017-06-30 09:55:36 -0700

    Web tests refactoring

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -1,70 +1,38 @@
-# -*- coding: utf-8 -*-
-
-import sys
 import unittest
-from flask import Flask
-from app import app,db
-from app.models import User, Party
 
-class test_login(unittest.TestCase):
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
-    TESTING = True
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        db.init_app(app)
-        with app.app_context():
-            db.create_all()
-            self.insert_data_to_db()
-        return app
+from app import app, db
 
-    def insert_data_to_db(self):
-        db.session.commit()
-        admon = User('tomer', 'admon', '123')
-        avoda = Party(u'העבודה',
-                      'https://www.am-1.org.il/wp-content/uploads/2015/03/%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94.-%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%99%D7%97%D7%A6.jpg')
-        db.session.add(avoda)
-        db.session.add(admon)
-        db.session.commit()
+SQLALCHEMY_DATABASE_URI = "sqlite://"
+TESTING = True
 
+class AppTestCase(unittest.TestCase):
     def setUp(self):
-        self.check =app.test_client()
-
-    def test_manager(self):
-        response = self.check.get('app/manager')
-        self.assertEqual(response.status_code, 404);
-
-    def test_for_missing_id(self):
-        # this test ensures that you cannot get an access without id number
-        invalid_login = self.check.post('login' , data = { 'first_name':'tomer' , 'last_name': 'admon'} ,  follow_redirects=True)
-        self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
-
-    def test_invalid_user(self):
-        invalid_user=self.check.post('login', data = { 'first_name':'sali' , 'last_name': 'impostor', 'id':'2407' } ,  follow_redirects=True)
-        return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data
-
-    # def test_customer_not_exist_in_db(self):
-    #     invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '0' ))
-    # #     assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
-
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.check = app.test_client(self)
 
     def tearDown(self):
         del self.check
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
+    def test_cant_access_without_login(self):
+        # check if the url of accessing index page without logging in is the same of the login page
+        response = self.check.get('/app/manager')
+        self.assertEqual(response.status_code, 404)
 
-if __name__ == '__main__':
+    def test_false_login(self):
+        # check that you can't login without id
+        invalid_login = self.check.post('login', data=dict(first_name='wrong', last_name='user'))
+        self.assertEqual(invalid_login.status_code, 400)
+
+    def test_false_user_id(self):
+        # check the error of user who's not in the database
+        invalid_login = self.check.post('login', data=dict(first_name='wrong', last_name='user', id='987'))
+        str = invalid_login.data.decode('utf-8')
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in str
+
+if (__name__ == '__main__'):
     unittest.main()
-
-
-
-
-
-
-
-
-
-
-
-
